df7e5231ac1eeca1e0393a107684053fae80f6a1
Elif Ersümer <elifersumer@gmail.com>
2021-09-28 01:19:22 +0300

    DependencyResolvers (Ninject)

---
+++ DipFramework.Business.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DipFramework.Business.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ DipFramework.Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,29 @@
+﻿using DipFramework.Business.Abstract;
+using DipFramework.Business.Concrete.Managers;
+using DipFramework.DataAccess.EntityFramework.Abstract;
+using DipFramework.DataAccess.EntityFramework.Concrete;
+using DipFramework.DataAccess.EntityFramework.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DipFramework.Business.DependencyResolvers.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().To<ProductManager>().InSingletonScope();
+            Bind<IProductDal>().To<EfProductDal>();
+
+            //Bind(typeof(IQueryableRepository<>)).To(typeof(EfQueryableRepositoryBase<>));
+            Bind<DbContext>().To<NorthwindContext>();
+
+            //Bind<NhibernateHelper>().To<SqlServerHelper>();
+        }
+    }
+}
